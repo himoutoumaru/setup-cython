32b998c46157f39ec7c2cc4eb6c6c49bf251e020
umaru <15875339926@139.com>
2019-05-19 11:50:38 +0800

    add flask test

--- setup_cython/bootstrap.py
+++ setup_cython/bootstrap.py
@@ -1,6 +1,19 @@
+import json
+from sklearn import svm
 from flask import Flask
+import psutil
+import numpy as np
+from sklearn import datasets
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    iris = datasets.load_iris()
+    digits = datasets.load_digits()
+    clf = svm.SVC(gamma=0.001, C=100.)
+    clf.fit(digits.data[:-1], digits.target[:-1])
+
+    arr = np.array([1, 2, 3])
+    return json.dumps(dict(cpu=str(psutil.cpu_freq()), np=arr.mean(), sci=str(clf.predict(digits.data[:-1]))))
