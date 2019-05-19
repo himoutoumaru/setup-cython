96f0ced4c995ef2a2db3f0f57f21cf348fb18ea2
umaru <15875339926@139.com>
2019-05-19 21:52:32 +0800

    add flask test

--- bin/main.py
+++ bin/main.py
@@ -1,7 +1,3 @@
-import multiprocessing
-
 from setup_cython.bootstrap import app
 
-if __name__ == '__main__':
-    multiprocessing.freeze_support()
-    app.run()
+app.run()
--- setup_cython/bootstrap.py
+++ setup_cython/bootstrap.py
@@ -4,7 +4,15 @@ import psutil
 import numpy as np
 from sklearn import linear_model
 
-app = Flask(__name__)
+import sys
+import os
+from flask import Flask, render_template
+
+if getattr(sys, 'frozen', False):
+    template_folder = os.path.join(sys._MEIPASS, 'templates')
+    app = Flask(__name__, template_folder=template_folder)
+else:
+    app = Flask(__name__)
 
 
 @app.route('/')
