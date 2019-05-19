ce1b378beac72c2d5fc3b8abc9c03abc8f6a9c2c
umaru <15875339926@139.com>
2019-05-19 22:01:39 +0800

    add flask test

--- setup_cython/bootstrap.py
+++ setup_cython/bootstrap.py
@@ -4,15 +4,7 @@ import psutil
 import numpy as np
 from sklearn import linear_model
 
-import sys
-import os
-from flask import Flask, render_template
-
-if getattr(sys, 'frozen', False):
-    template_folder = os.path.join(sys._MEIPASS, 'templates')
-    app = Flask(__name__, template_folder=template_folder)
-else:
-    app = Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route('/')
